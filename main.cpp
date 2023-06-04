9311a07c1aadaa946c9a3ed0a37dba5469fb8ea9
SergeyShirmankin <saransk_sega@mail.ru>
2023-06-04 16:26:08 +0300

    Sorting  the main file

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
 #include  <iostream>
-#include "Trie.h"
+#include "Source.h"
 using namespace std;
 int main()
 {
-	// Input keys (use only 'a' through 'z'
-	// and lower case)
-	string keys[] = { "the", "a", "there",
-					"answer", "any", "by",
-					"bye", "their" };
-	int n = sizeof(keys) / sizeof(keys[0]);
-	int n1 = sizeof(keys);
-	struct TrieNode* root = getNode();
-
-	// Construct trie
-	for (int i = 0; i < n; i++)
-		insert(root, keys[i]);
-
-	// Search for different keys
-	search(root, "the") ? cout << "Yes\n" :
-		cout << "No\n";
-	search(root, "these") ? cout << "Yes\n" :
-		cout << "No\n";
-	search(root, "their") ? cout << "Yes\n" :
-		cout << "No\n";
-	search(root, "thaw") ? cout << "Yes\n" :
-		cout << "No\n";
+	
+	Test();
 	return 0;
 }
